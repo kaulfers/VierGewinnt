7ca2b0a634700ffcf0f500f29f39641c23cd7ae8
Konrad Ceniuch <konradceniuch@icloud.com>
2024-05-20 18:59:07 +0200

    update app.java

--- src/gui/main/App.java
+++ src/gui/main/App.java
@@ -8,7 +8,6 @@ import java.awt.*;
 /**
  * Entry point for the application.
  * This class sets up the gui.frames JFrame to display the MainFrame JPanel, which contains the game interface.
- * ITS CURRENTLY UNDER CONSTRUCTION.
  */
 public class App {
     public static void main(String[] args) {
