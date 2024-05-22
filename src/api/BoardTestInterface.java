dec55bf4a1bb247d227c8e0075bba36ac3d4a4b3
IJasperI <jasper.mross@web.de>
2024-05-22 12:12:04 +0200

    Hinzufügen Funktionen zur Speicherung des Boards sowie Einlesen von Werten aus der gespeicherten Datei

--- src/api/BoardTestInterface.java
+++ src/api/BoardTestInterface.java
@@ -6,12 +6,15 @@ public interface BoardTestInterface {
     void placeStone(int column); // diese Methode legt einen Stein, überprüft den Status und wechselt den Spieler
     void checkStatus(int column); // überprüft den Status des Spiels, ob gewonnen oder voll
     void changePlayer(); // wechselt den Spieler
+    void saveBoard(); //speichert das Board als String in eine Textdatei
+    void overwriteVariableWithSavestats(); //übernimmt die Werte aus dem savefile(Werte, die angepasst werden:board,isFull,isPlayer1sTurn)
     void setTurn(boolean player1sTurn); // setzt welcher Spieler ist am Zug (True = Spieler1, False = Spieler2)
     void setBoard(Tile[][] board); // setzt das Spielfeld als 2-dimensionalem Array aus Kacheln(Tiles)
     void setColumns(int columns); //setzt die Anzahl der Spalten
     void setrows(int rows); //setzt die Anzahl der Reihen
     void setWhoHasWon(int whoWon); //0: niemand     1: Spieler1     2:Spieler2
     void setIsFull(boolean isFull); // setzt den Status ob das Spielfeld voll ist
+    void setBoard(Tile[][]); //ersetzt board mit dem input-Array
     boolean getTurn(); // gibt den Spieler der am Zug ist wieder (True = Spieler1, False = Spieler2)
     Tile[][] getBoard(); // gibt das Spielfeld als 2-dimensionalem Array aus Kacheln(Tiles) wieder
     int getColumns(); //gibt die Anzahl der Spalten
--- src/logic/Board.java
+++ src/logic/Board.java
@@ -11,6 +11,7 @@ public class Board implements BoardInterface,BoardTestInterface {
     Tile[][] board; // das Spielfeld 	//Array[rows][columns]
     int whoHasWon; //0: niemand     1: Spieler1     2:Spieler2
     boolean isFull; // wenn das Spielfeld voll ist und keiner gewonnen hat
+    final private String savepath = "savestats.txt"; //filepath + name, where the savefile is placed
 
     //vollständiger Konstruktor
     public Board(boolean iP1T, int columns, int rows, int wHW, boolean iF){
@@ -36,8 +37,6 @@ public class Board implements BoardInterface,BoardTestInterface {
         isFull = false;
         fillBoard();
     }
-
-    //test
 
     private Board(Board board) {
         int rows = board.board.length;
@@ -375,6 +374,268 @@ public class Board implements BoardInterface,BoardTestInterface {
     }
 
     /**
+     * Saves the Board-Class with all their variables in a textfile.
+     * 
+     */
+    @Overwrite
+	public void saveBoard(){
+
+		File savefile = new File(this.savepath);
+		String savecode;
+		
+		//löschen des alten savefiles
+		if (savefile.exists()){
+			savefile.delete();
+		}
+		
+		//erstellen speicherfile, wenn noch nicht vorhanden
+		try{
+			
+			if (savefile.createNewFile() ){
+				System.out.println("File created in: '" + this.savepath + "'");
+			}
+			else{
+				System.out.println("File '" + this.savepath + "' exist.");
+			}
+			
+		} catch(Exception e){
+			//throw new IOException("File konnte nicht erstellt werden.");
+			System.err.println(e);
+		}
+
+		//erstellen speichercode
+		savecode = this.createSavecode();
+		
+		//schreiben des speichercodes in die speicherfile
+		try{ 
+			FileWriter fw = new FileWriter(savefile);
+			fw.write(savecode);
+			fw.close();						//schließen der speicherfile
+		} catch (Exception e){ 
+			 e.getStackTrace();
+		}
+	
+	}
+
+    /**
+     * Creates a String, which contains all the information of the Board
+     * 
+     * Example: 2a3a4a0aB0100201101
+     * a is a seperator for the numbers
+     * 2 -> which players turn it is   | isPlayer1sTurn
+     * 3 -> rows
+     * 4 -> columns
+     * 0 -> value of isFull            | isFull
+     * number after 'B' -> status of Tile from top left to bottom right | board
+     * 
+     */
+	private String createSavecode(){
+		int row = getrows();
+		int column = getColumns();
+		String seperator = "a";
+		
+		String output = "";
+		
+		//hinzufügen von playerturn
+		if (this.isPlayer1sTurn){
+			output += "1";
+		}
+		else{
+			output += "2";
+		}
+		output += seperator;		//seperator einfügen
+ 
+		//hinzufügen von row
+		output += Integer.toString(row);
+		output += seperator;		//seperator einfügen
+		
+		//hinzufügen von column
+		output += Integer.toString(column);
+		output += seperator;		//seperator einfügen
+		
+		//hinzufügen von isFull
+		if (this.isFull){
+			output += "1";
+		}
+		else{
+			output += "0";
+		}
+		output += seperator;		//seperator einfügen
+		
+		
+		//einfügen des Boards
+		output += "B";		//seperator einfügen
+		
+		for(Tile[] r: this.board){
+			for(Tile c: r){
+				output += c.toString();
+			}
+		}
+		
+		return output;
+		
+		
+	}
+
+    /**
+     * Overwrites the variables of the Board with the values from the savefile
+     * 
+     */
+    @Overwrite
+    public void overwriteVariableWithSavestats(){
+		String code = getSavecodeFromFile();
+		
+		if (!isSavecodeValid(code)){
+			System.out.println("Speichercode ist fehlerhaft.");
+			return;
+		}
+		
+		setValuesFromSavecode(code);
+		
+		
+	}
+
+    /**
+     * gets the param from the savecode and puts them into the variables of the instance
+     */
+	private void setValuesFromSavecode(String savecode){
+		Tile[][] spFeld;
+		boolean player1Turn = false;
+		boolean full = false;
+		int row = -1;
+		int column = -1;
+		
+		char[] scCh = savecode.toCharArray();	//string zu array von chars
+		int countA = 0;
+		String v = "";
+		boolean checkB = false;
+		int r=0;
+		int c=0;
+		
+		//iteration über jeden einzelnen char von String savecode
+		for (char ch : scCh){
+			//wenn a
+			if (ch == 'a'){
+				
+				switch(countA){
+				case 0:
+					if (v == "1"){
+						player1Turn = true;
+					}
+					else if (v == "2"){
+						player1Turn = false;
+					}
+					break;
+				case 1:
+					row = Integer.parseInt(v);
+					break;
+				case 2:
+					column = Integer.parseInt(v);
+					break;
+				case 3:
+					if (v == "1"){
+						full = true;
+					}
+					else if (v == "0"){
+						full = false;
+					}
+					break;
+				default:
+					System.out.println("countA in werteVonSavecode ist größer als 3: " + countA);
+					break;
+				}
+				
+				countA += 1;
+				v = "";		//zurücksetzen von v
+			}
+			//wenn B
+			else if (ch == 'B'){
+				break;
+			}
+			else{
+				v += Character.toString(ch);
+			}
+		}
+    //füllen des spielfeldVektors
+		spFeld = new Tile[row][column];
+		for (char ch : scCh){
+			
+			if (checkB == false){
+				if (ch == 'B'){
+					checkB = true;
+
+				}
+			}
+			else{
+
+				spFeld[r][c] = Character.getNumericValue(ch);
+				
+				if (c < column-1){
+					c += 1;
+				}
+				else{
+					c = 0;
+					r +=1;
+				}
+
+			}
+			
+		}
+		
+		this.setBoard(spFeld);
+        this.setIsFull(full);
+        this.setTurn(player1Turn);
+	}
+
+    /**
+     * Reads the Savefile and retrieves the code from it.
+     * @return the String in the savefile
+     */
+	private String getSavecodeFromFile(){
+		
+		File savefile = new File(savepath);
+		FileReader fr = null;
+		int ch;
+		String output = "";
+		
+		// check if File exists or not 
+		try
+		{ 
+			fr = new FileReader(savefile);
+		} 
+		catch (FileNotFoundException fe) 
+		{ 
+			System.out.println("File not found"); 
+		} 
+		
+		// read from FileReader till the end of file 
+		try{
+			while ((ch=fr.read())!=-1) 
+				output += (char)ch;
+			
+			fr.close();		//close the file 
+		} catch(Exception e){
+			System.err.println(e);
+		} 
+
+	
+		return output;
+	}
+
+    /**
+     * Checks, if the given String is useable as Savecode.
+     * @return if the String can be used as Savecode
+     */
+    private boolean isSavecodeValid(String code){
+		
+		Pattern savecodePattern = Pattern.compile("[1-2]a[0-9]+a[0-9]+a[0-1]aB[0-2]*");	//factory, Builder-Pattern -> verhindert falsches Instanziieren; compile .. funktion, die das instanziieren übernimmt-
+		Matcher matcher = savecodePattern.matcher(code);
+		
+		return matcher.find();
+	}
+
+
+    /**
     *    Setzt den Status, ob das Spielfeld voll ist oder nicht.
     *   @param isFull True, wenn das Spielfeld voll ist, ansonsten False.
     */
@@ -392,6 +653,23 @@ public class Board implements BoardInterface,BoardTestInterface {
     public Tile[][] getBoard(){
         return this.board;
     }
+
+    /**
+     * overwrites the board with a given input array
+     */
+    @Overwrite
+    public void setBoard(Tile[][] value){
+		int row = value.length;
+		int column = value[0].length;
+		
+		this.board = new Tile[row][column];
+		
+		for (int r=0;r<row;r++){
+			for (int c=0;c<column;c++){
+				this.board[r][c] = value[r][c];
+			}
+		}
+	}
 
     @Override
     public int getrows(){
