5e6fdf1465971e509f9dfcf8c909091de8f0299c
osqr <oski2004pl@gmail.com>
2024-05-25 19:16:54 +0200

    :)

--- src/gui/main/App.java
+++ src/gui/main/App.java
@@ -9,6 +9,7 @@ import java.awt.*;
  * Entry point for the application.
  * This class sets up the gui.frames JFrame to display the MainFrame JPanel, which contains the game interface.
  */
+
 public class App {
     public static void main(String[] args) {
         new StartFrame();
